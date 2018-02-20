82de844cb591001597da3dba9d6c5fd832e35da6
cyb3rn3t1c1an <alexeypashkevich@gmail.com>
2018-02-20 08:32:06 +0300

    Added task-3

---
+++ task-3/js/const.js
@@ -0,0 +1,9 @@
+const HUMAN = 1;
+const HUMAN_ICON = "twirl#blueIcon";
+const HUMAN_COLOR = "#0000FF";
+
+const COMPUTER = 2;
+const COMPUTER_ICON = "twirl#redIcon";
+const COMPUTER_COLOR = "#FF0000";
+
+const DEFAULT_ZOOM = 6;
