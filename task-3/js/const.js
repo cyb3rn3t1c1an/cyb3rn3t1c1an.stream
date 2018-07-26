0120bce8526188dd9c44f01cb2159f143261f086
cyb3rn3t1c1an <alexeypashkevich@gmail.com>
2018-07-26 15:42:41 +0300

    Made cleanup

--- task-3/js/const.js
+++
@@ -1,9 +0,0 @@
-const HUMAN = 1;
-const HUMAN_ICON = "twirl#blueIcon";
-const HUMAN_COLOR = "#0000FF";
-
-const COMPUTER = 2;
-const COMPUTER_ICON = "twirl#redIcon";
-const COMPUTER_COLOR = "#FF0000";
-
-const DEFAULT_ZOOM = 6;
